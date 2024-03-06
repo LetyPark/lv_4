8114267fe92cd878fc150cf18f27c0fe8f6e0290
friend-0228 <kwj2712@gamil.com>
2024-03-06 19:51:22 +0900

    auth미들웨어 수정

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,73 +1,61 @@
 import jwt from 'jsonwebtoken';
 import { prisma } from '../utils/prisma/index.js';
 
-// 원래 비밀키는 .env 파일을 이용해 관리해야 한다.
-const ACCESS_TOKEN_SECRET_KEY = 'access-secret-key';
-const REFRESH_TOKEN_SECRET_KEY = 'refresh-secret-key';
-
-const tokenStorages = {};
-
 export default async function (req, res, next) {
     try {
-        const { accessToken, refreshToken } = req.cookies;
+        const { accessToken } = req.cookies;
+
         // 400 토큰이 존재하지 않는 경우
-        if (!accessToken || !refreshToken) {
-            return res
-                .status(400)
-                .json({ errorMessage: 'Access Token 또는 Refresh Token이 존재하지 않습니다.' });
+        if (!accessToken) {
+            return res.status(400).json({ errorMessage: 'Access Token 이 존재하지 않습니다.' });
         }
-        console.log('🎫🎫🎫' + accessToken);
-        console.log('🎟️🎟️🎟️' + refreshToken);
 
-        tokenStorages[refreshToken] = {
-            nickname: req.nickname,
-            ip: req.ip,
-            userAgent: req.headers['user-agent'],
-        };
-
+        // 액세스 토큰 형식이 Bearer 형식이 아닌 경우
         const [accessTokenType, accessTokenValue] = accessToken.split(' ');
-        const [refreshTokenType, refreshTokenValue] = refreshToken.split(' ');
-        if (accessTokenType !== 'Bearer' || refreshTokenType !== 'Bearer') {
+        if (accessTokenType !== 'Bearer') {
             res.clearCookie();
             return res.status(400).json({ errorMessage: '토큰 타입이 Bearer 형식이 아닙니다.' });
         }
 
-        // jwt 토큰 검증
-        const decodedAccessToken = jwt.verify(accessTokenValue, ACCESS_TOKEN_SECRET_KEY);
-        const decodedRefreshToken = jwt.verify(refreshTokenValue, REFRESH_TOKEN_SECRET_KEY);
-
-        // 인증에 실패하거나 해독된 토큰이 이 없을 때
-        if (!decodedAccessToken || !decodedRefreshToken) {
-            return res.status(401).json({ errorMessage: '토큰이 정상적이지 않습니다.' });
-        }
-        console.log('🎫🎫🎫' + decodedAccessToken);
-        console.log('🎟️🎟️🎟️' + decodedRefreshToken);
-
         const { nickname, role } = decodedAccessToken;
 
+        let decodedAccessToken;
+        let decodedRefreshToken;
+        try {
+            // 액세스 토큰 검증
+            decodedAccessToken = jwt.verify(accessTokenValue, 'access-secret-key');
+        } catch (error) {
+            // 액세스 토큰이 만료된 경우
+            if (error.name === 'TokenExpiredError') {
+                return res.status(401).json({ errorMessage: '토큰이 만료되었습니다.' });
+            }
+            // 리프레시 토큰 검증
+            const { refreshToken } = req.cookies;
+
+            decodedRefreshToken = jwt.verify(refreshToken, 'refresh-secret-key');
+
+            if (!decodedRefreshToken) {
+                return res.status(401).json({ errorMessage: '리프레시 토큰이 유효하지 않습니다.' });
+            }
+
+            // 액세스 토큰이 만료되었을 경우, 새로운 액세스 토큰 발급
+            const newAccessToken = jwt.sign({ nickname, role }, 'access-secret-key', {
+                expiresIn: '10m',
+            });
+            res.cookie('accessToken', newAccessToken, { httpOnly: true });
+            // 다음 미들웨어로 진행
+            return next();
+        }
+
+        // 사용자 확인
         const user = await prisma.users.findFirst({ where: { nickname } });
         if (!user) throw new Error('사용자가 존재하지 않습니다.');
 
         req.user = user;
         req.role = role;
 
-        // 액세스 토큰이 만료되었을 경우, 새로운 액세스 토큰 발급
-        const userInfo = tokenStorages[refreshToken];
-        if (!userInfo) {
-            return res
-                .status(419)
-                .json({ errorMessage: 'Refresh Token의 정보가 서버에 존재하지 않습니다.' });
-        }
-
-        const newAccessToken = jwt.sign(decodedAccessToken, ACCESS_TOKEN_SECRET_KEY);
-
-        res.cookie('accessToken', `Bearer ${newAccessToken}`);
-
         next();
     } catch (error) {
-        if (error.name === 'TokenExpiredError') {
-            return res.status(401).json({ errorMessage: '토큰이 만료되었습니다.' });
-        }
         if (error.name === 'JsonWebTokenError') {
             return res.status(401).json({ errorMessage: '토큰이 조작되었습니다.' });
         }
